26bfcde1f90a3f6f25ee5104abca2d5c218819a2
GabrielC <gabriel.lcifba@gmail.com>
2025-10-14 11:10:07 -0300

    fix: responsiveness - min-width for main

--- src/app/components/PageContent/index.tsx
+++ src/app/components/PageContent/index.tsx
@@ -6,7 +6,10 @@ interface PageContentProps {
 
 export function PageContent({ children }: PageContentProps) {
   return (
-    <main className="ml-64 p-6 flex flex-col gap-y-8 mt-[var(--page-header-height)]">
+    <main
+      style={{ minWidth: "calc(1024px - var(--sidebar-width))" }}
+      className="ml-[var(--sidebar-width)] p-6 flex flex-col gap-y-8 mt-[var(--page-header-height)]"
+    >
       {children}
     </main>
   );
--- src/app/components/PageHeader/index.tsx
+++ src/app/components/PageHeader/index.tsx
@@ -2,7 +2,7 @@ import { SectionTitle } from "@/app/components/SectionTitle";
 
 export function PageHeader() {
   return (
-    <header className="fixed top-0 w-full ml-64 bg-[var(--bg-light)] border-b border-[var(--border-light)] p-4 flex items-center justify-between dark:bg-[var(--bg-dark)] dark:border-[var(--border-dark)] h-[var(--page-header-height)] z-10">
+    <header className="fixed top-0 w-full ml-[var(--sidebar-width)] bg-[var(--bg-light)] border-b border-[var(--border-light)] p-4 flex items-center justify-between dark:bg-[var(--bg-dark)] dark:border-[var(--border-dark)] h-[var(--page-header-height)] z-10">
       <SectionTitle>Dashboard de Monitoramento</SectionTitle>
     </header>
   );
