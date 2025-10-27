d35411406195cce5d6701df5c2e52c36e086d06c
GabrielC <gabriel.lcifba@gmail.com>
2025-10-27 10:16:35 -0300

    refactor: modularização da tabela de logs

--- src/app/components/LogsTable/index.tsx
+++ src/app/components/LogsTable/index.tsx
@@ -1,5 +1,9 @@
+import { Pill } from "@/app/components/Pill";
+import { Td } from "@/app/components/LogsTable/Td";
+import { Th } from "@/app/components/LogsTable/Th";
 import { formatarData } from "@/utils/helpers/formatar";
 import { converterTempo } from "@/utils/helpers/converter";
+import { obterCorMetodo, obterCorStatusCode } from "@/utils/helpers/obterCor";
 
 interface Log {
   id: number;
@@ -17,58 +21,19 @@ interface LogsTableProps {
 }
 
 export function LogsTable({ logs }: LogsTableProps) {
-  const getMethodColor = (method: string) => {
-    switch (method) {
-      case "GET":
-        return "bg-blue-100 text-blue-800";
-      case "POST":
-        return "bg-green-100 text-green-800";
-      case "PUT":
-        return "bg-yellow-100 text-yellow-800";
-      case "DELETE":
-        return "bg-red-100 text-red-800";
-      default:
-        return "bg-gray-100 text-gray-800";
-    }
-  };
-
-  const getStatusCodeColor = (statusCode: number) => {
-    if (statusCode >= 200 && statusCode < 300) {
-      return "bg-green-100 text-green-800";
-    } else if (statusCode >= 400 && statusCode < 500) {
-      return "bg-yellow-100 text-yellow-800";
-    } else {
-      return "bg-red-100 text-red-800";
-    }
-  };
-
   return (
     <div className="bg-bg-light dark:bg-bg-dark rounded-lg shadow overflow-hidden border border-border-light dark:border-border-dark">
       <div className="overflow-x-auto">
         <table className="min-w-full divide-y divide-border-light dark:divide-border-dark">
           <thead className="bg-gray-50 dark:bg-gray-700">
             <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                IP
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                Método
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                Endpoint
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                Status
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                Data
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                Hora
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
-                Duração
-              </th>
+              <Th>IP</Th>
+              <Th>Método</Th>
+              <Th>Endpoint</Th>
+              <Th>Status</Th>
+              <Th>Data</Th>
+              <Th>Hora</Th>
+              <Th>Duração</Th>
             </tr>
           </thead>
           <tbody className="divide-y divide-border-light dark:divide-border-dark">
@@ -77,39 +42,21 @@ export function LogsTable({ logs }: LogsTableProps) {
                 key={log.id}
                 className="hover:bg-gray-50 dark:hover:bg-gray-700"
               >
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-mono">
-                  {log.ip}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <span
-                    className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${getMethodColor(
-                      log.http_method
-                    )}`}
-                  >
+                <Td className="text-sm font-mono">{log.ip}</Td>
+                <Td>
+                  <Pill className={obterCorMetodo(log.http_method)}>
                     {log.http_method}
-                  </span>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-mono">
-                  {log.endpoint}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <span
-                    className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${getStatusCodeColor(
-                      log.status_code
-                    )}`}
-                  >
+                  </Pill>
+                </Td>
+                <Td className="text-sm font-mono">{log.endpoint}</Td>
+                <Td>
+                  <Pill className={obterCorStatusCode(log.status_code)}>
                     {log.status_code}
-                  </span>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm">
-                  {formatarData(log.data)}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm">
-                  {log.hora.slice(0, 5)}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm">
-                  {converterTempo(log.duracao)}
-                </td>
+                  </Pill>
+                </Td>
+                <Td className="text-sm">{formatarData(log.data)}</Td>
+                <Td className="text-sm">{log.hora.slice(0, 5)}</Td>
+                <Td className="text-sm">{converterTempo(log.duracao)}</Td>
               </tr>
             ))}
           </tbody>
