7762a2a272fa9f9a8937632d846438ca33f2b8f7
zolppy <zolppy.me@gmail.com>
2026-05-21 10:37:02 -0300

    fix: exibição de gráficos quando carregando ou sem dados

--- components/Charts/OneLine.tsx
+++ components/Charts/OneLine.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@heroui/react";
+import { Card, Skeleton } from "@heroui/react";
 import {
   CartesianGrid,
   Legend,
@@ -35,57 +35,61 @@ function OneLineChart({
   return (
     <>
       {isLoading ? (
-        <span className="text-center self-center">Carregando...</span>
-      ) : data?.length > 0 ? (
+        <Skeleton className="h-80 rounded-3xl" />
+      ) : (
         <Card>
           <Card.Header>
             <Card.Title className="text-lg font-bold">{label}</Card.Title>
           </Card.Header>
           <Card.Content>
-            <LineChart
-              style={{
-                width: "100%",
-                maxWidth: "700px",
-                maxHeight: "500px",
-                aspectRatio: 1.618,
-              }}
-              responsive
-              data={data}
-              margin={{ top: 50, right: 5, left: 5 }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey={dataKey} hide={hideXAxis} />
-              <YAxis width="auto" />
-              <Tooltip
-                itemStyle={{}}
-                contentStyle={{
-                  backgroundColor: "#1f2937",
-                  border: "1px solid #4b5563",
-                  borderRadius: "5px",
-                  color: "#f9fafb",
+            {data.length > 0 ? (
+              <LineChart
+                style={{
+                  width: "100%",
+                  maxWidth: "700px",
+                  maxHeight: "500px",
+                  aspectRatio: 1.618,
                 }}
-                labelStyle={{}}
-                wrapperStyle={{ borderRadius: "10px" }}
-                separator=": "
-                cursor={{
-                  fillOpacity: 0.5,
-                  fill: lineColor,
-                }}
-              />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey={lineDataKey}
-                stroke={lineColor}
-                activeDot={{ fill: activeDotColor }}
-                label={{ position: "top", fontSize: 12 }}
-                name={name}
-              />
-            </LineChart>
+                responsive
+                data={data}
+                margin={{ top: 50, right: 5, left: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey={dataKey} hide={hideXAxis} />
+                <YAxis width="auto" />
+                <Tooltip
+                  itemStyle={{}}
+                  contentStyle={{
+                    backgroundColor: "#1f2937",
+                    border: "1px solid #4b5563",
+                    borderRadius: "5px",
+                    color: "#f9fafb",
+                  }}
+                  labelStyle={{}}
+                  wrapperStyle={{ borderRadius: "10px" }}
+                  separator=": "
+                  cursor={{
+                    fillOpacity: 0.5,
+                    fill: lineColor,
+                  }}
+                />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey={lineDataKey}
+                  stroke={lineColor}
+                  activeDot={{ fill: activeDotColor }}
+                  label={{ position: "top", fontSize: 12 }}
+                  name={name}
+                />
+              </LineChart>
+            ) : (
+              <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                Sem dados disponíveis.
+              </p>
+            )}
           </Card.Content>
         </Card>
-      ) : (
-        <span className="text-center self-center">Sem dados disponíveis.</span>
       )}
     </>
   );
--- components/Charts/VerticalBar.tsx
+++ components/Charts/VerticalBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Bar, BarChart, Tooltip, XAxis, YAxis } from "recharts";
-import { Card } from "@heroui/react";
+import { Card, Skeleton } from "@heroui/react";
 
 interface ChartBarProps {
   data: Record<string, any>[];
@@ -33,70 +33,74 @@ function VerticalBarChart({
   return (
     <>
       {isLoading ? (
-        <span className="text-center self-center">Carregando...</span>
-      ) : data?.length > 0 ? (
+        <Skeleton className="h-80 rounded-3xl" />
+      ) : (
         <Card>
           <Card.Header>
             <Card.Title className="text-lg font-bold">{label}</Card.Title>
           </Card.Header>
           <Card.Content>
-            <BarChart
-              layout={layout}
-              style={{
-                width: "100%",
-                maxWidth: "700px",
-                maxHeight: "500px",
-                aspectRatio: 1.618,
-              }}
-              responsive
-              data={data}
-              margin={{ top: 50, right: 5, left: 5 }}
-            >
-              <XAxis
-                type={layout === "vertical" ? "number" : "category"}
-                dataKey={layout === "horizontal" ? dataKey : undefined}
-                hide={hideXAxis}
-                width="auto"
-              />
-              <YAxis
-                type={layout === "vertical" ? "category" : "number"}
-                dataKey={layout === "vertical" ? dataKey : undefined}
-                hide={hideYAxis}
-                width="auto"
-              />
-              <Tooltip
-                itemStyle={{}}
-                contentStyle={{
-                  backgroundColor: "#1f2937",
-                  border: "1px solid #4b5563",
-                  borderRadius: "5px",
-                  color: "#f9fafb",
+            {data.length > 0 ? (
+              <BarChart
+                layout={layout}
+                style={{
+                  width: "100%",
+                  maxWidth: "700px",
+                  maxHeight: "500px",
+                  aspectRatio: 1.618,
                 }}
-                labelStyle={{}}
-                wrapperStyle={{ borderRadius: "10px" }}
-                separator=": "
-                cursor={{
-                  fillOpacity: 0.5,
-                  fill: barColor,
-                }}
-              />
-              <Bar
-                dataKey={barDataKey}
-                fill={barColor}
-                activeBar={{ fill: activeBarColor }}
-                label={{
-                  position: layout === "vertical" ? "right" : "top",
-                  fontSize: 12,
-                }}
-                radius={layout === "vertical" ? [0, 5, 5, 0] : [5, 5, 0, 0]}
-                maxBarSize={75}
-                name={name}
-              />
-            </BarChart>
+                responsive
+                data={data}
+                margin={{ top: 50, right: 5, left: 5 }}
+              >
+                <XAxis
+                  type={layout === "vertical" ? "number" : "category"}
+                  dataKey={layout === "horizontal" ? dataKey : undefined}
+                  hide={hideXAxis}
+                  width="auto"
+                />
+                <YAxis
+                  type={layout === "vertical" ? "category" : "number"}
+                  dataKey={layout === "vertical" ? dataKey : undefined}
+                  hide={hideYAxis}
+                  width="auto"
+                />
+                <Tooltip
+                  itemStyle={{}}
+                  contentStyle={{
+                    backgroundColor: "#1f2937",
+                    border: "1px solid #4b5563",
+                    borderRadius: "5px",
+                    color: "#f9fafb",
+                  }}
+                  labelStyle={{}}
+                  wrapperStyle={{ borderRadius: "10px" }}
+                  separator=": "
+                  cursor={{
+                    fillOpacity: 0.5,
+                    fill: barColor,
+                  }}
+                />
+                <Bar
+                  dataKey={barDataKey}
+                  fill={barColor}
+                  activeBar={{ fill: activeBarColor }}
+                  label={{
+                    position: layout === "vertical" ? "right" : "top",
+                    fontSize: 12,
+                  }}
+                  radius={layout === "vertical" ? [0, 5, 5, 0] : [5, 5, 0, 0]}
+                  maxBarSize={75}
+                  name={name}
+                />
+              </BarChart>
+            ) : (
+              <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                Sem dados disponíveis.
+              </p>
+            )}
           </Card.Content>
         </Card>
-      ) : (
-        <span className="text-center self-center">Sem dados disponíveis.</span>
       )}
     </>
   );
