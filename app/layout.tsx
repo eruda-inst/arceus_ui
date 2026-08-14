e8e6f072ee6943d76e483dbe7c80717754563f9f
Zolppy <zolppy.me@gmail.com>
2026-08-14 16:17:26 -0300

    feat: adicionadas configurações de viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { fontInter } from "@/configs/font.config";
 import Providers from "@/app/providers";
 import "@/app/globals.css";
@@ -9,14 +9,21 @@ export const metadata: Metadata = {
     "Plataforma para monitoramento de requisições HTTP realizadas ao Arceus, exibição de métricas e dashboards.",
 };
 
+export const viewport: Viewport = {
+  themeColor: "#000000",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" className={`dark bg-black h-full antialiased ${fontInter.variable}`}>
-      <body >
+    <html
+      lang="pt-br"
+      className={`dark bg-black h-full antialiased ${fontInter.variable}`}
+    >
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
